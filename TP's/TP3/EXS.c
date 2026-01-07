10e7e58219b3e8d9cad19cb5606fd3d99bfe9aa0
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-07 18:10:28 +0100

    Q5

--- TP's/TP3/EXS.c
+++ TP's/TP3/EXS.c
@@ -51,8 +51,7 @@ void main(){
     LATD = 0x00;
     ANSELB = 0x00;
     
-    INTCON2 |= 0x40; // INTEDG0=1
-    INTCON2 |= 0x20; // INTEDG1=1
+    INTCON2 |= 0x60; // INTEDG0=1, INTEDG1=1
 
     INTCON |= 0x90; // GIE=1, INT0IE=1
     INTCON3 |= 0x08; // INT1IE=1
@@ -87,5 +86,69 @@ void interrupt(){
     if( ( (INTCON3 >> 0) & 1) == 1){ // Si INT1F=1
         int1_flag = 1;
         INTCON3 &= 0xFE; // INT1F=0
+    }
+}
+
+//---------------------| Q4 |---------------------//
+void main(){
+    TRISA = 0x00;
+    LATA = 0x00;
+
+    TRISB = 0xF0;
+    ANSELB = 0x00;
+
+    INTCON |= 0x88; // GIE=1, RBIE=1
+    INTCON2 &= 0xFE; // RBIF=0
+
+    while(1);
+}
+
+void interrupt(){
+    if( (INTCON >> 0) & 1){ // Si RBIF=1
+        LATA = ~LATA;
+        INTCON &= 0xFE; // RBIF=0
+    }
+}
+
+//---------------------| Q5 |---------------------//
+volatile char var1, var2;
+
+void main(){
+    TRISA = 0xFF;
+    TRISC = 0xFF;
+
+    TRISD = 0x00;
+    LATD = 0x00;
+    ANSELB = 0x00;
+
+    RCON |= 0x80; // IPEN=1
+    INTCON |= 0xC0; // GIEH=1, GIEL=1
+
+    INTCON |= 0x10; // INT0IE=1
+    INTCON3 |= 0x18; // INT1IE=1, INT2IE=1
+
+    INTCON &= 0xFD; // INT0IF=0
+    INTCON3 &= 0xFC; // INT1IF=0, INT2IF=0
+
+    INTCON2 |= 0x70; // INTEDG0=1, INTEDG1=1, INTEDG2=1
+
+    while(1){
+        LATD = var1 + var2;
+    }
+}
+
+void interrupt(){
+    if( (INTCON >> 1) & 1){ // Si INT0IF=1
+        var1 = var2 = 0;
+        INTCON &= 0xFD; // INT0IF=0
+    }
+    if( (INTCON3 >> 0) & 1){ // Si INT1IF=1
+        var1 = PORTC;
+        INTCON3 &= 0xFE; // INT1IF=0
+    }
+
+    if( (INTCON3 >> 1) & 1){ // Si INT2IF=1
+        var2 = PORTA;
+        INTCON3 &= 0xFD; // INT2IF=0
     }
 }
