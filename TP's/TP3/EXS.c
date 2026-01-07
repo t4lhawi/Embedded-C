822307563d99f6b70a68344d2aa97d2400aa44c4
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-07 08:58:28 +0100

    PREMIER QUESTION

---
+++ TP's/TP3/EXS.c
@@ -0,0 +1,23 @@
+// Nom : Mohamed Talhaoui
+// Filière : GSEIR3 (S1) | 2025/2026
+// mikroC PRO for PIC (PIC18F)
+
+//---------------------| Q1 |---------------------//
+void main(){
+    TRISC = 0;
+    ANSELB = 0;
+    INTCON2 |= 0x40; // INTEDG0=1
+    INTCON |= 0x90; // GIE=1, INT0IE=1
+    
+    while(1){
+        LATC = 0x00;
+    }
+}
+
+void interrupt(){
+    LATC=~LATC;
+    delay_ms(1000);
+    INTCON &=0xFD; // INT0F=0
+}
+
+//---------------------| Q2 |---------------------//
