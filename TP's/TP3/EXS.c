e6e74a5bb2871400d4f81c1bf4bf764b2e2d0124
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-07 10:00:41 +0100

    Q2

--- TP's/TP3/EXS.c
+++ TP's/TP3/EXS.c
@@ -16,8 +16,27 @@ void main(){
 
 void interrupt(){
     LATC=~LATC;
-    delay_ms(1000);
+    delay_ms(500);
     INTCON &=0xFD; // INT0F=0
 }
 
 //---------------------| Q2 |---------------------//
+void main(){
+    TRISC = 0;
+    TRISD = 0;
+    ANSELB = 0;
+    INTCON2 |= 0x40; // INTEDG0=1
+    INTCON |= 0x88; // GIE=1, RBIE=1
+    
+    while(1){
+        LATC = 0x00;
+    }
+}
+
+void interrupt(){
+    LATC=~LATC;
+    delay_ms(1000);
+    LATD = LATC;
+    INTCON &=0xFC; // RBIF=0
+}
+//---------------------| Q3 |---------------------//
