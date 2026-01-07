8f9c7b84ba858d25286d6aa0ca017394c79e2e53
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-07 13:08:38 +0100

    ajouter ex3

--- TP's/TP3/EXS.c
+++ TP's/TP3/EXS.c
@@ -4,8 +4,9 @@
 
 //---------------------| Q1 |---------------------//
 void main(){
-    TRISC = 0;
-    ANSELB = 0;
+    TRISC = 0x00;
+    ANSELB = 0x00;
+
     INTCON2 |= 0x40; // INTEDG0=1
     INTCON |= 0x90; // GIE=1, INT0IE=1
     
@@ -15,16 +16,17 @@ void main(){
 }
 
 void interrupt(){
-    LATC=~LATC;
+    LATC = ~LATC;
     delay_ms(500);
-    INTCON &=0xFD; // INT0F=0
+    INTCON &= 0xFD; // INT0F=0
 }
 
 //---------------------| Q2 |---------------------//
 void main(){
-    TRISC = 0;
-    TRISD = 0;
-    ANSELB = 0;
+    TRISC = 0x00;
+    TRISD = 0x00;
+    ANSELB = 0x00;
+
     INTCON2 |= 0x40; // INTEDG0=1
     INTCON |= 0x88; // GIE=1, RBIE=1
     
@@ -34,9 +36,56 @@ void main(){
 }
 
 void interrupt(){
-    LATC=~LATC;
+    LATC = ~LATC;
     delay_ms(1000);
     LATD = LATC;
-    INTCON &=0xFC; // RBIF=0
+    INTCON &= 0xFC; // RBIF=0
 }
+
 //---------------------| Q3 |---------------------//
+volatile char int0_flag = 0;
+volatile char int1_flag = 0;
+
+void main(){
+    TRISD = 0x00;
+    LATD = 0x00;
+    ANSELB = 0x00;
+    
+    INTCON2 |= 0x40; // INTEDG0=1
+    INTCON2 |= 0x20; // INTEDG1=1
+
+    INTCON |= 0x90; // GIE=1, INT0IE=1
+    INTCON3 |= 0x08; // INT1IE=1
+    
+    INTCON &= 0xFD; // INT0F=0
+    INTCON3 &= 0xFE; // INT1F=0
+
+
+    while(1){
+        if(int0_flag){ // Si INT0F=1
+            LATD = 0x01;
+            delay_ms(1000);
+            LATD = 0x00;
+            int0_flag = 0;
+        }
+
+        if(int1_flag){ // Si INT1F=1
+            LATD = 0x02;
+            delay_ms(4000);
+            LATD = 0x00;
+            int1_flag = 0;
+        }
+    }
+}
+
+void interrupt(){
+    if( ( (INTCON >> 1) & 1) == 1){ // Si INT0F=1
+        int0_flag = 1;
+        INTCON &= 0xFD; // INT0F=0
+    }
+
+    if( ( (INTCON3 >> 0) & 1) == 1){ // Si INT1F=1
+        int1_flag = 1;
+        INTCON3 &= 0xFE; // INT1F=0
+    }
+}
