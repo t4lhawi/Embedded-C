828ac4e90cf8e054142733e3f912ae0283a9bef3
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-11 11:16:38 +0100

    Ajouter exercice 3

--- TP's/TP4/DIS0123.c
+++ TP's/TP4/DIS0123.c
@@ -22,32 +22,56 @@ void interrupt() {
     portd_index++ ;
     if (portd_index > 3) portd_index = 0;
     // reset TIMER0 data register value
+    TMR0L = 0;
+    TMR0H = 0;
     // Clear TMR0IF
+    INTCON &= 0xFB;
 }
 
 void main(){
     // Configure PORTA as output
+    TRISA = 0x00;
     // Clear PORTA
+    LATA = 0;
     // Configure PORTD as output
+    TRISD = 0x00;
     // Clear PORTD
+    LATD = 0;
     // Set TMR0 mode, and assign prescaler to TMR0 to create a delay of 1 ms
-    // clear TMROL
+    T0CON = 0b11000100; // TMR0ON = 1, T08BIT = 1, T0CS = 0, PSA = 0, T0PS = 0b100 (Pré-diviseur = 1:32)
+    TMR0L = 194; // Valeur initial
+    // clear TMR0IF
+    INTCON |= 0x04;
     // active Timer0 interruption
+    INTCON |= 0xA0;
+
     digit = 0;
     portd_index = 0;
     shifter = 1;
     number = 9950; // Initial number value
     do {
-        // extract thousands from number an put it in digit
+        // extract thousands from number an put it in digit        
+        digit = (number / 1000) % 10;
         // and store the corresponding mask to PORTD array index 3
+        portd_array[3] = mask(digit);
         // extract hundreds from number an put it in digit
+        digit = digit = (number / 100) % 10;
         // and store the corresponding mask to PORTD array index 2
+        portd_array[2] = mask(digit);
         // extract tens from number an put it in digit
+        digit = digit = (number / 10) % 10;
         // and store the corresponding mask to PORTD array index 1
+        portd_array[1] = mask(digit);
         // extract ones from number an put it in digit
+        digit = digit = (number / 1) % 10;
         // and store the corresponding mask to PORTD array index 0
+        portd_array[0] = mask(digit);
+
         delay_ms(1000);
+
         number++ ;
+
         if (number > 9999) number = 9950;
+
     } while(1);
 }
