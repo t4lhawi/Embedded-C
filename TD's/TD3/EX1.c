f94d004eae4fc58dfefc7d92ec9697b41c656331
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2026-01-12 14:57:02 +0100

    corriger ex1

--- TD's/TD3/EX1.c
+++ TD's/TD3/EX1.c
@@ -2,27 +2,25 @@
 // Filière : GSEIR3 (S1) | 2025/2026
 // mikroC PRO (Compiler) | Microcontrôleurs PIC18F (Microchip)
 
-unsigned char etat = 0;
-
 void main(){
-    TRISC = 0x00;
+    TRISB |= 0x10; // RB4
+    ANSELB = 0;
+    TRISC &= 0xF3;
     LATC = 0x00;
 
-    INTCON2 |= 0x40; // INTEDG0=1
-    INTCON |= 0x90; // GIE=1, INT0IE=1
+    INTCON |= 0x88; // GIE=1, RBIE=1
+    INTCON &= 0xFE; // RBIF=0
+    IOCB = 0x10; // IOCB4=1
 
-    while(1){
-        if(!etat){
-            LATC = 0x01; // Interrupteur Ouvert
-        } else {
-            LATC = 0x02; // Interrupteur Fermé
-        }
-    }
+    while(1);
 }
 
 void interrupt(){
-    if( (INTCON >> 1) & 1){ // Si INT0IF=1
-        etat = !etat;
-        INTCON &= 0xFD; // INT0F=0
+    if(PORTB & 0x10){ // RB4=1 (Interrupteur Fermé)
+        LATC = 0x02;
     }
+    else { // RB4=0 (Interrupteur Ouvert)
+        LATC = 0x01;
+    }
+    INTCON &= 0xFE; // RBIF=0
 }
